daaee5d5a3ba97430378360c4bdb3932321a8f32
Bojidar Dimitrov <bojidar.dimitrov00@gmail.com>
2022-01-12 14:50:22 +0200

    Add Common layer

---
+++ FileTransferSystem/FileTransferSystem.Common/Configuration/UserConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace FileTransferSystem.Common.Configuration
+{
+    public class UserConfiguration
+    {
+        public string GrantType { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
